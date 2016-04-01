45c53aeb4c0d127d34d0eda706a37184182e4c09
Tristan Burgess <bistan.turgess@gmail.com>
2016-03-31 20:39:26 -0400

    feat: Initial commit of magic balls demo

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+
+    public float translateMagnitude = 3.0f;
+    public float gravityMagnitude = 9.81f;
+
+    private CharacterController playerController;
+
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Debug.Log("Horizontal Input = " + Input.GetAxis("Horizontal"));
+        Debug.Log("Vertical Input = " + Input.GetAxis("Vertical"));
+
+        Vector3 movementZ = Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * translateMagnitude;
+        Vector3 movementX = Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * translateMagnitude;
+
+        Vector3 movement = transform.TransformDirection(movementZ + movementX);
+
+        movement.y -= gravityMagnitude * Time.deltaTime;
+
+        // Debug.Log("Movement Vector = " + movement);
+
+        playerController.Move(movement);
+    }
+}
---
+++ Assets/Scripts/OnDeathZoneTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnDeathZoneTrigger : MonoBehaviour {
+    
+    void OnTriggerEnter(Collider other)
+    {
+        // only do stuff if hit by a projectile
+        if (other.tag == "Player")
+        {
+            // call the NextLevel function in the game manager
+            GameManager.gm.RestartLevel();
+        }
+    }
+}
